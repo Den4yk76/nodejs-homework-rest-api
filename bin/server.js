290caf496bb6db20c34ae60fc6c40ee56a265fde
Denys Nazarenko <denysnazarenko23@gmail.com>
2021-12-21 15:14:11 +0200

    Done to Step-4 (Connection to MongoDB), without change CRUD-operations methods

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,42 @@
-const app = require('../app')
+const app = require('../app');
+const mongoose = require('mongoose');
 
-const PORT = process.env.PORT || 3000
+const URL =
+  'mongodb+srv://Den4yk76:Den4yk76@cluster0.wdqol.mongodb.net/db-contacts?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`)
-})
+// const contactsSchema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: [true, 'Set name for contact'],
+//   },
+//   email: {
+//     type: String,
+//   },
+//   phone: {
+//     type: String,
+//   },
+//   favorite: {
+//     type: Boolean,
+//     default: false,
+//   },
+// });
+
+// const Contacts = await mongoose.model('contacts', contactsSchema);
+// const FindedContacts = await Contacts.find();
+// console.log(FindedContacts);
+
+const start = async () => {
+  try {
+    await mongoose.connect(URL, { useNewUrlParser: true });
+    console.log('Database connection successful');
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+start();
