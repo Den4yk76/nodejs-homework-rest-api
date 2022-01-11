128e722ef4b9895314723b3e889fc89ce061dbfb
Denys Nazarenko <denysnazarenko23@gmail.com>
2022-01-11 17:01:48 +0200

    started writing login tests

---
+++ __tests__/loginTests.js
@@ -0,0 +1,5 @@
+import { login } from '../controllers/auth/index';
+
+describe('login controller tests', () => {
+    it('should return status 200, token, user email and subscription', () => {});
+});
