f02c01c61738ee9c50a6ccb540d4ef91e251e501
Denys Nazarenko <denysnazarenko23@gmail.com>
2022-01-12 20:06:44 +0200

    hw-5 done without tests. will do it after hw check

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,103 +1,103 @@
 import { HttpCode } from '../../lib/constants.js';
 import AuthService from '../../service/auth/';
 import {
-    UploadFileService,
-    LocalFileStorage,
+  UploadFileService,
+  LocalFileStorage,
 } from '../../service/file-storage';
 const authService = new AuthService();
 
 const registration = async (req, res, next) => {
-    const { email } = req.body;
-    const isUserExist = await authService.isUserExist(email);
-    if (isUserExist) {
-        return res.status(HttpCode.CONFLICT).json({
-            status: 'error',
-            code: HttpCode.CONFLICT,
-            message: 'Email in use',
-        });
-    }
-    const data = await authService.create(req.body);
-
-    res.status(HttpCode.CREATED).json({
-        status: 'success',
-        code: HttpCode.CREATED,
-        user: data,
+  const { email } = req.body;
+  const isUserExist = await authService.isUserExist(email);
+  if (isUserExist) {
+    return res.status(HttpCode.CONFLICT).json({
+      status: 'error',
+      code: HttpCode.CONFLICT,
+      message: 'Email in use',
     });
+  }
+  const data = await authService.create(req.body);
+
+  res.status(HttpCode.CREATED).json({
+    status: 'success',
+    code: HttpCode.CREATED,
+    user: data,
+  });
 };
 
 const login = async (req, res, next) => {
-    const { email, password } = req.body;
-    const user = await authService.getUser(email, password);
+  const { email, password } = req.body;
+  const user = await authService.getUser(email, password);
 
-    if (!user) {
-        return res.status(HttpCode.UNAUTHORIZED).json({
-            status: 'error',
-            code: HttpCode.UNAUTHORIZED,
-            message: 'Email or password is wrong',
-        });
-    }
-    const token = authService.getToken(user);
-    await authService.setToken(user.id, token);
-
-    const { subscription } = user;
-    res.status(HttpCode.OK).json({
-        status: 'success',
-        code: HttpCode.OK,
-        data: { token, user: { email, subscription } },
+  if (!user) {
+    return res.status(HttpCode.UNAUTHORIZED).json({
+      status: 'error',
+      code: HttpCode.UNAUTHORIZED,
+      message: 'Email or password is wrong',
     });
+  }
+  const token = authService.getToken(user);
+  await authService.setToken(user.id, token);
+
+  const { subscription } = user;
+  res.status(HttpCode.OK).json({
+    status: 'success',
+    code: HttpCode.OK,
+    data: { token, user: { email, subscription } },
+  });
 };
 
 const logout = async (req, res, next) => {
-    await authService.setToken(req.user.id, null);
-    res.status(HttpCode.NO_CONTENT).json({
-        status: 'success',
-        code: HttpCode.NO_CONTENT,
-        data: {},
-    });
+  await authService.setToken(req.user.id, null);
+  res.status(HttpCode.NO_CONTENT).json({
+    status: 'success',
+    code: HttpCode.NO_CONTENT,
+    data: {},
+  });
 };
 
 const currentUser = async (req, res, next) => {
-    const { email, subscription } = req.user;
-    res.status(HttpCode.OK).json({
-        status: 'success',
-        code: HttpCode.OK,
-        data: { email, subscription },
-    });
+  const { email, subscription } = req.user;
+  res.status(HttpCode.OK).json({
+    status: 'success',
+    code: HttpCode.OK,
+    data: { email, subscription },
+  });
 };
 
 const updateSubscription = async (req, res, next) => {
-    const { email, id } = req.user;
-    const { subscription } = req.body;
-    await authService.setSubscription(id, subscription);
+  const { email, id } = req.user;
+  const { subscription } = req.body;
+  await authService.setSubscription(id, subscription);
 
-    res.status(HttpCode.OK).json({
-        status: 'success',
-        code: HttpCode.OK,
-        data: { email, subscription },
-    });
+  res.status(HttpCode.OK).json({
+    status: 'success',
+    code: HttpCode.OK,
+    data: { email, subscription },
+  });
 };
 
 const uploadAvatar = async (req, res, next) => {
-    const uploadService = new UploadFileService(
-        LocalFileStorage,
-        req.file,
-        req.user,
-    );
+  const uploadService = new UploadFileService(
+    LocalFileStorage,
+    req.file,
+    req.user,
+  );
 
-    const avatarUrl = await uploadService.updateAvatar();
+  const avatarUrl = await uploadService.updateAvatar();
 
-    res.status(HttpCode.OK).json({
-        status: 'success',
-        code: HttpCode.OK,
-        avatarUrl,
-    });
+  res.status(HttpCode.OK).json({
+    status: 'success',
+    code: HttpCode.OK,
+    avatarUrl,
+  });
 };
 
 export {
-    registration,
-    login,
-    logout,
-    currentUser,
-    updateSubscription,
-    uploadAvatar,
+  registration,
+  login,
+  logout,
+  currentUser,
+  updateSubscription,
+  uploadAvatar,
 };
